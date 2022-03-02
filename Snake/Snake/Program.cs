e72d8a603527ec551fbd67f686c757b1b594c219
Igor Khilman <men17@list.ru>
2022-03-02 20:48:55 +0300

    Осваиваем класс «Список»

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,6 +16,15 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
 
             Console.ReadLine();
         }
