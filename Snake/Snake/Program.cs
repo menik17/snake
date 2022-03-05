e03652ad47f903ec15c35f8a7af00d47edd52309
Igor Khilman <men17@list.ru>
2022-03-05 23:44:15 +0300

    Класс «Змейка»

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -23,7 +23,9 @@ namespace Snake
             leftLine.Drow();
             rightLine.Drow();
 
-
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
             Console.ReadLine();
         }
     }
